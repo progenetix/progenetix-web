d8027cecf53568fc19b0a29464d44ce7b8643e65
Michael Baudis <m@baud.is>
2020-07-13 10:45:16 +0200

    remove dataset specification from `useFilteringTerms` template
    See also https://github.com/ptoussai/progenetix-next/issues/11 for how this should be handled.

--- src/api/bycon.js
+++ src/api/bycon.js
@@ -11,7 +11,7 @@ export function useDatasets() {
 
 export function useFilteringTerms(prefixes) {
   return useSWR(
-    `${basePath}cgi/bycon/bin/byconplus.py/filtering_terms?prefixes=${prefixes}&datasetIds=progenetix`
+    `${basePath}cgi/bycon/bin/byconplus.py/filtering_terms?prefixes=${prefixes}`
   )
 }
 
