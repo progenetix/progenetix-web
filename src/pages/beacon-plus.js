98c737f657920db60d4e6f7dd5104b84982474b9
Pierre-Henri Toussaint <pht@mailbox.org>
2020-07-15 09:02:24 +0200

    Rework of request type selectors to fix #10

--- src/pages/beacon-plus.js
+++ src/pages/beacon-plus.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react"
 import { useForm } from "react-hook-form"
 import requestTypesConfig from "../../config/beacon-plus/requestTypes.yaml"
-import examplesConfig from "../../config/beacon-plus/examples.yaml"
 import { useBeaconQuery } from "../api/bycon"
 import { markdownToReact } from "../utils/md"
 import Nav from "../components/Nav"
 import { BeaconForm } from "../components/beacon-plus/BeaconForm"
 import { DatasetResultBox } from "../components/beacon-plus/DatasetResultBox"
+import cn from "classnames"
 
 export default function BeaconPlus() {
   const {
@@ -15,11 +15,14 @@ export default function BeaconPlus() {
     errors,
     reset,
     setError,
+    setValue,
     clearErrors
   } = useForm()
   const [query, setQuery] = useState(null) // actual valid query
-  // could be the example of the requestType as they have the same shape and effect
-  const [requestConfig, setRequestConfig] = useState(null)
+
+  const [requestType, setRequestType] = useState(
+    Object.entries(requestTypesConfig)[0][0] // auto select first requestType from the file
+  )
   const {
     data: queryResponse,
     error: queryError,
@@ -28,95 +31,67 @@ export default function BeaconPlus() {
 
   const isLoading = !queryResponse && !queryError && !!query
 
-  const onSubmit = (formValues) => {
-    clearErrors()
-    // At this stage individual parameters are already validated.
-    const {
-      requestType,
-      variantType,
-      referenceBases,
-      alternateBases,
-      start,
-      end
-    } = formValues
+  const requestConfig = requestTypesConfig[requestType]
 
-    const missingParameters = []
+  const onSubmit = handleFormSubmit(
+    clearErrors,
+    setError,
+    mutateQuery,
+    setQuery
+  )
 
-    if (requestType === "variantAlleleRequest") {
-      if (!referenceBases || !alternateBases || !start) {
-        missingParameters.push("referenceBases", "alternateBases", "start")
-      }
-    } else if (requestType === "variantCNVrequest") {
-      if (!start || !end || !variantType) {
-        missingParameters.push("start", "end", "variantType")
-      }
-    } else if (requestType === "variantRangeRequest") {
-      if (variantType && (referenceBases || alternateBases)) {
-        const error = {
-          type: "manual",
-          message: "Use either Variant Type or Ref. Base(s) and Alt. Base(s)."
-        }
-        setError("variantType", error)
-        setError("referenceBases", error)
-        setError("alternateBases", error)
-        return
-      }
-      if (!variantType && !(referenceBases || alternateBases)) {
-        missingParameters.push(
-          "variantType",
-          "referenceBases",
-          "alternateBases"
-        )
-      }
-    } else if (requestType === "variantFusionRequest") {
-      //
-    }
-    if (missingParameters.length > 0) {
-      missingParameters.forEach((name) =>
-        setError(name, { type: "manual", message: "Parameter is missig" })
-      )
-      return
-    }
-
-    mutateQuery(null) // mutateQuery and clear current results
-    setQuery(formValues)
-  }
-
-  const setRequestType = (requestType) => {
-    reset({})
-    setRequestConfig(requestType)
-  }
-
-  const setExample = (example) => {
+  const handleRequestTypeClicked = (requestTypeId) => {
     const newParams = Object.fromEntries(
-      Object.entries(example.parameters).map(([k, v]) => [k, v.examplevalue])
+      Object.entries(
+        requestTypesConfig[requestTypeId].parameters
+      ).map(([k, v]) => [k, v?.value])
     )
-
     reset(newParams)
-    setRequestConfig(example)
+    setRequestType(requestTypeId)
   }
+
+  const handleExampleClicked = (example) =>
+    Object.entries(example.parameters).forEach(([k, v]) => setValue(k, v.value))
 
   return (
     <>
       <Nav />
       <section className="section">
         <div className="container mb-5">
-          <div className="notification is-info is-light">
-            {requestConfig?.description ? (
-              markdownToReact(requestConfig?.description)
-            ) : (
-              <span>
-                This forward looking Beacon interface implements additional,
-                planned features beyond the current Beacon specifications.
-                Further information can be found through the{" "}
-                <a href="http://beacon-project.io/">ELIXIR Beacon website</a>.
-              </span>
-            )}
+          <div className="content mb-6">
+            This forward looking Beacon interface implements additional, planned
+            features beyond the current Beacon specifications. <br />
+            Further information can be found through the{" "}
+            <a href="http://beacon-project.io/">ELIXIR Beacon website</a>.
           </div>
-          <RequestConfig
-            setExample={setExample}
-            setRequestType={setRequestType}
-          />
+          <div className="tabs is-fullwidth">
+            <ul>
+              {Object.entries(requestTypesConfig).map(([id, value]) => (
+                <li
+                  className={cn({ "is-active": id === requestType })}
+                  key={id}
+                  onClick={() => handleRequestTypeClicked(id)}
+                >
+                  <a>{value.label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="content">
+            {requestConfig.description &&
+              markdownToReact(requestConfig?.description)}
+          </div>
+          <div className="buttons">
+            {Object.entries(requestConfig.examples || []).map(([id, value]) => (
+              <button
+                key={id}
+                className="button is-light "
+                onClick={() => handleExampleClicked(value)}
+              >
+                {value.label}
+              </button>
+            ))}
+          </div>
           <BeaconForm
             requestConfig={requestConfig}
             handleSubmit={handleSubmit}
@@ -130,35 +105,6 @@ export default function BeaconPlus() {
           <Results response={queryResponse} error={queryError} query={query} />
         </div>
       </section>
-    </>
-  )
-}
-
-function RequestConfig({ setRequestType, setExample }) {
-  return (
-    <>
-      <div className="buttons">
-        {Object.entries(requestTypesConfig).map(([id, value]) => (
-          <button
-            key={id}
-            className="button is-light is-info"
-            onClick={() => setRequestType(value)}
-          >
-            {value.label}
-          </button>
-        ))}
-      </div>
-      <div className="buttons">
-        {Object.entries(examplesConfig).map(([id, value]) => (
-          <button
-            key={id}
-            className="button is-light"
-            onClick={() => setExample(value)}
-          >
-            {value.label}
-          </button>
-        ))}
-      </div>
     </>
   )
 }
@@ -215,4 +161,60 @@ function QuerySummary({ query }) {
       <span>Filters: {filters.join(", ")}</span>
     </>
   )
+}
+
+function handleFormSubmit(clearErrors, setError, mutateQuery, setQuery) {
+  return (formValues) => {
+    clearErrors()
+    // At this stage individual parameters are already validated.
+    const {
+      requestType,
+      variantType,
+      referenceBases,
+      alternateBases,
+      start,
+      end
+    } = formValues
+
+    const missingParameters = []
+
+    if (requestType === "variantAlleleRequest") {
+      if (!referenceBases || !alternateBases || !start) {
+        missingParameters.push("referenceBases", "alternateBases", "start")
+      }
+    } else if (requestType === "variantCNVrequest") {
+      if (!start || !end || !variantType) {
+        missingParameters.push("start", "end", "variantType")
+      }
+    } else if (requestType === "variantRangeRequest") {
+      if (variantType && (referenceBases || alternateBases)) {
+        const error = {
+          type: "manual",
+          message: "Use either Variant Type or Ref. Base(s) and Alt. Base(s)."
+        }
+        setError("variantType", error)
+        setError("referenceBases", error)
+        setError("alternateBases", error)
+        return
+      }
+      if (!variantType && !(referenceBases || alternateBases)) {
+        missingParameters.push(
+          "variantType",
+          "referenceBases",
+          "alternateBases"
+        )
+      }
+    } else if (requestType === "variantFusionRequest") {
+      //
+    }
+    if (missingParameters.length > 0) {
+      missingParameters.forEach((name) =>
+        setError(name, { type: "manual", message: "Parameter is missing" })
+      )
+      return
+    }
+
+    mutateQuery(null) // mutateQuery and clear current results
+    setQuery(formValues)
+  }
 }
