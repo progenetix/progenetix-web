5d65ba4df73fcfbff39fddbb71f208ce9da5bb51
Michael Baudis <m@baud.is>
2020-09-10 13:31:05 +0200

    Fast fix Publication map legend

--- src/modules/publications/PublicationsListPage.js
+++ src/modules/publications/PublicationsListPage.js
@@ -196,12 +196,9 @@ function PublicationsContainer({ publications }) {
         <PublicationsMap publications={publications} height={400} />
       </div>
       <p className="content">
-        The map displays the geographic distribution (by corresponding author)
-        of the <b>{acghSum}</b> genomic array, <b>{ccghSum}</b> chromosomal CGH
-        and <b>{wesSum + wgsSum}</b> whole genome/exome based cancer genome
-        datasets. The numbers are derived from the <b>{publicationsCount}</b>{" "}
+        The map displays the geographic distribution of the currently selected
         publications registered in the Progenetix database. Area sizes
-        correspond to the sample numbers reported from a given location.
+        correspond to the publication numbers reported from a given location.
       </p>
     </>
   )
