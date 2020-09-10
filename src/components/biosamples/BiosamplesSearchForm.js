ca48a371c0cb6f4a5820af06f0f5cd5de2f30aef
Pierre-Henri Toussaint <pht@mailbox.org>
2020-09-10 13:38:14 +0200

    Extract GeoCitySelector

--- src/components/biosamples/BiosamplesSearchForm.js
+++ src/components/biosamples/BiosamplesSearchForm.js
@@ -2,7 +2,6 @@ import cn from "classnames"
 import {
   INTEGER_RANGE_REGEX,
   useCollations,
-  useGeoCity,
   validateBeaconQuery
 } from "../../hooks/api"
 import React, { useEffect, useMemo, useState } from "react"
@@ -20,7 +19,7 @@ import SelectField from "../form/SelectField"
 import InputField from "../form/InputField"
 import useDeepCompareEffect from "use-deep-compare-effect"
 import { withUrlQuery } from "../../hooks/url-query"
-import { useAsyncSelect } from "../../hooks/asyncSelect"
+import { GeoCitySelector } from "./GeoCitySelector"
 
 export const BiosamplesSearchForm = withUrlQuery(
   ({ urlQuery, setUrlQuery, ...props }) => (
@@ -543,40 +542,5 @@ function FilterLogicWarning({ isVisible }) {
     >
       Multiple term selected !
     </span>
-  )
-}
-
-function GeoCitySelector({
-  name,
-  label,
-  control,
-  errors,
-  register,
-  className
-}) {
-  const { inputValue, onInputChange } = useAsyncSelect()
-  const { data, isLoading } = useGeoCity({ city: inputValue })
-  let options = []
-  if (data) {
-    options = data.map((g) => ({
-      value: g.city,
-      data: g,
-      label: `${g.city} (${g.country})`
-    }))
-  }
-  return (
-    <SelectField
-      name={name}
-      label={label}
-      isLoading={isLoading && !!inputValue}
-      options={options}
-      onInputChange={onInputChange}
-      control={control}
-      errors={errors}
-      register={register}
-      className={className}
-      useOptionsAsValue
-      isClearable
-    />
   )
 }
