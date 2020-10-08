12fa0b81ed920b154417c5c59097f69fe27a94ab
Pierre-Henri Toussaint <pht@mailbox.org>
2020-10-08 21:40:16 +0200

    Better /services/ontologymaps

--- src/pages/service-collection/ontologymaps.js
+++ src/pages/service-collection/ontologymaps.js
@@ -1,48 +1,113 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Layout } from "../../components/layouts/Layout"
-import { basePath, useExtendedSWR } from "../../hooks/api"
-import { useAsyncSelect } from "../../hooks/asyncSelect"
+import { basePath, useCollations, useExtendedSWR } from "../../hooks/api"
 import CustomSelect from "../../components/Select"
+import { Loader } from "../../components/Loader"
+import { withUrlQuery } from "../../hooks/url-query"
 
-export default function Ontologymaps() {
-  const { inputValue, onInputChange, value, onChange } = useAsyncSelect()
-  const values = (value && value.map((option) => option.value)) ?? []
+const Ontologymaps = withUrlQuery(({ urlQuery, setUrlQuery }) => {
+  const allOntologiesOptions = useAllOntologiesOptions()
 
-  let filters
-  if (values.length === 0) {
-    filters = inputValue
-  } else {
-    filters = values.join(",")
+  const [firstSelection, setFirstSelection] = useState(urlQuery.firstSelection)
+  const handleFirstSelectionChange = (firstSelection) => {
+    setUrlQuery({ firstSelection }, { keepExisting: true })
+    setSecondSelection(null)
+    setFirstSelection(firstSelection)
   }
+  useEffect(() => {
+    setFirstSelection(urlQuery.firstSelection)
+  }, [urlQuery.firstSelection])
 
-  const { data, isLoading, error } = useOntologymaps({ filters })
+  const [secondSelection, setSecondSelection] = useState(
+    urlQuery.secondSelection
+  )
+  const handleSecondSelectionChange = (secondSelection) => {
+    setUrlQuery({ secondSelection }, { keepExisting: true })
+    setSecondSelection(secondSelection)
+  }
+  useEffect(() => {
+    setSecondSelection(urlQuery.secondSelection)
+  }, [urlQuery.secondSelection])
 
-  const options = getOptions(data)
+  // compute second selection options
+  const {
+    isLoading: secondSelectionLoading,
+    error: secondSelectionError,
+    options: secondSelectionOptions
+  } = useGetFilteredOptions({
+    filters: firstSelection,
+    remove: firstSelection
+  })
+
+  // compute result
+  const selections = [firstSelection, secondSelection].filter((s) => !!s)
+  let filters
+  if (selections.length === 0) {
+    filters = selections
+  } else {
+    filters = selections.join(",")
+  }
+  const {
+    data: resultsData,
+    isLoading: resultsLoading,
+    error: resultsError
+  } = useOntologymaps({ filters })
 
   return (
     <Layout title="Ontologymaps" headline="Ontologymaps">
-      <div>
-        {error && (
-          <div className="notification is-warning">Could not load data...</div>
-        )}
+      <div className="mb-6">
         <CustomSelect
-          className="mb-6"
-          options={options}
-          isLoading={!!inputValue && isLoading}
-          onInputChange={onInputChange}
-          value={value}
-          onChange={onChange}
-          placeholder="Ex: icdom-81403..."
-          isMulti
+          className="mb-5"
+          options={allOntologiesOptions}
+          value={
+            allOntologiesOptions.find((o) => o.value === firstSelection) ?? null
+          }
+          onChange={(option) => handleFirstSelectionChange(option?.value)}
           isClearable
+          placeholder="First selection"
         />
-        {data && data.code_groups && (
-          <CodeGroups codeGroups={data && data.code_groups} />
+        {firstSelection && (
+          <Loader
+            isLoading={secondSelectionLoading}
+            hasError={secondSelectionError}
+          >
+            {secondSelectionOptions && secondSelectionOptions.length ? (
+              <CustomSelect
+                className="mb-6"
+                options={secondSelectionOptions}
+                value={
+                  secondSelectionOptions.find(
+                    (o) => o.value === secondSelection
+                  ) ?? null
+                }
+                onChange={(option) =>
+                  handleSecondSelectionChange(option?.value)
+                }
+                isClearable
+                placeholder="Second selection"
+              />
+            ) : (
+              <div className="notification">
+                No groups found for the first selection.
+              </div>
+            )}
+          </Loader>
+        )}
+        {secondSelection && (
+          <Loader isLoading={resultsLoading} hasError={resultsError}>
+            {resultsData?.code_groups.length > 0 ? (
+              <CodeGroups codeGroups={resultsData?.code_groups} />
+            ) : (
+              <div className="notification">No groups found.</div>
+            )}
+          </Loader>
         )}
       </div>
     </Layout>
   )
-}
+})
+
+export default Ontologymaps
 
 function getOptions(data) {
   if (!data || data.unique_codes == null) return []
@@ -58,23 +123,52 @@ function getOptions(data) {
 function CodeGroups({ codeGroups }) {
   return (
     <div className="content">
-      <h5>Code groups</h5>
-      {codeGroups.map((codeGroup, i) => (
-        <ul key={i}>
-          {codeGroup.map((code) => (
-            <li key={code.id}>
-              {code.id}: {code.label}
-            </li>
-          ))}
-        </ul>
-      ))}
+      <h5>Selected codes</h5>
+      <table className="table is-bordered">
+        {codeGroups.map((codeGroup, i) => (
+          <tr key={i}>
+            {codeGroup.map((code) => (
+              <td key={code.id}>
+                {code.id}: {code.label}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </table>
     </div>
   )
 }
 
+function useAllOntologiesOptions() {
+  const { data } = useCollations({
+    filters: "",
+    method: "counts",
+    datasetIds: "progenetix"
+  })
+  if (!data) return []
+
+  return data
+    .filter(
+      (c) =>
+        c.id.includes("icdot") ||
+        c.id.includes("icdom") ||
+        c.id.includes("NCIT")
+    )
+    .map((c) => ({
+      label: c.id,
+      value: c.id
+    }))
+}
+
 function useOntologymaps({ filters }) {
   const url =
-    filters &&
+    filters?.length > 0 &&
     `${basePath}services/ontologymaps/?filters=${filters}&responseFormat=simplelist`
   return useExtendedSWR(url)
+}
+function useGetFilteredOptions({ filters, remove }) {
+  const { data, isLoading, error } = useOntologymaps({ filters })
+  let options = getOptions(data)
+  options = options.filter((o) => o.value !== remove)
+  return { isLoading, error, options }
 }
