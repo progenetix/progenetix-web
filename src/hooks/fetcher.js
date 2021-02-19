ea9099fb7a0d036ac729033f8e027ae0f07f7a58
Pierre-Henri Toussaint <pht@mailbox.org>
2021-02-19 14:24:22 +0100

    Improve fetching error handling

--- src/hooks/fetcher.js
+++ src/hooks/fetcher.js
@@ -5,7 +5,12 @@ export default async (url) => {
   if (!res.ok) {
     const error = new Error("An error occurred while fetching the data.")
     // Attach extra info to the error object.
-    error.info = await res.json()
+
+    try {
+      error.info = await res.json()
+    } catch (err) {
+      //ignore
+    }
     error.status = res.status
     throw error
   }
