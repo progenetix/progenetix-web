15f811efe66fd8f8a3a04e6975967e8eb5f8c7a8
mbaudis <mbaudis@me.com>
2021-04-28 18:06:55 +0200

    refactoring Beacon+ navigation

--- src/modules/beacon-instances/1000-genomes-cnv/BeaconPage.js
+++ src/modules/beacon-instances/1000-genomes-cnv/BeaconPage.js
@@ -1,13 +1,14 @@
 import React from "react"
 import parametersConfig from "../shared/searchParameters.yaml"
 import requestTypesConfig from "./requestTypes.yaml"
-import Nav from "./Nav"
+import BeaconPlusNav from "../../../components/BeaconPlusNav"
+// import Nav from "./Nav"
 import BiosamplesSearchPanel from "../../../components/biosamples/BiosamplesSearchPanel"
 
 export default function ThousandGenomesPage({ cytoBands }) {
   return (
     <>
-      <Nav />
+      <BeaconPlusNav beaconName="1kG CNV" />
       <div className="section">
         <div className="BeaconPlus__container">
           <BiosamplesSearchPanel
--- src/modules/beacon-instances/beacon-plus/BeaconPage.js
+++ src/modules/beacon-instances/beacon-plus/BeaconPage.js
@@ -1,14 +1,14 @@
 import React from "react"
 import parametersConfig from "../shared/searchParameters.yaml"
 import requestTypesConfig from "./requestTypes.yaml"
-import Nav from "./Nav"
+import BeaconPlusNav from "../../../components/BeaconPlusNav"
 import BiosamplesSearchPanel from "../../../components/biosamples/BiosamplesSearchPanel"
 
 
 export default function BeaconPlusPage({ cytoBands }) {
   return (
     <>
-      <Nav />
+      <BeaconPlusNav beaconName="" />
       <div className="section">
         <div className="BeaconPlus__container">
           <BiosamplesSearchPanel
