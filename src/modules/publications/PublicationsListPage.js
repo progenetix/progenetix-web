9bfdbceeb5325ce282afe6f60e531cf708df8a6c
Pierre-Henri Toussaint <pht@mailbox.org>
2021-05-10 10:28:29 +0200

    Sort publications by id

--- src/modules/publications/PublicationsListPage.js
+++ src/modules/publications/PublicationsListPage.js
@@ -112,20 +112,35 @@ function PublicationsLoader({ geoCity, geodistanceKm, textSearch }) {
   )
 }
 
+function getPublicationIdNumber(publicationId) {
+  return +publicationId.substring(
+    publicationId.indexOf(":") + 1,
+    publicationId.length
+  )
+}
+
 function PublicationTable({ publications }) {
   const publicationsCount = publications.length
+  const sortType = React.useMemo(
+    () => (rowA, rowB, id) => {
+      const idA = getPublicationIdNumber(rowA.original[id])
+      const idB = getPublicationIdNumber(rowB.original[id])
+      return idA > idB ? 1 : idB > idA ? -1 : 0
+    },
+    []
+  )
   const columns = React.useMemo(
     () => [
       {
         Header: `Publications (${publicationsCount})`,
         columns: [
-          { accessor: "sortid" },
           {
             Header: InfodotHeader(
               "id",
               "Publication id (PubMed) with link to details page"
             ),
             accessor: "id",
+            sortType,
             Cell: function Cell(cellInfo) {
               return (
                 <a href={`/publications/details?id=${cellInfo.value}`}>
@@ -192,7 +207,7 @@ function PublicationTable({ publications }) {
       { accessor: "abstract" },
       { accessor: "title" }
     ],
-    [publicationsCount]
+    [publicationsCount, sortType]
   )
   return (
     <Table
@@ -200,6 +215,7 @@ function PublicationTable({ publications }) {
       data={publications}
       pageSize={25}
       hiddenColumns={["authors", "abstract", "sortid", "title"]}
+      sortBy={[{ id: "id", desc: true }]}
     />
   )
 }
