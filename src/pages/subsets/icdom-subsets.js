5a1895552312cadc39a73efa352cfd7d489f0998
Michael Baudis <mbaudis@me.com>
2023-02-17 17:39:57 +0100

    cleaning out icd pages

--- src/pages/subsets/icdom-subsets.js
+++
@@ -1,2 +0,0 @@
-import Page from "../../modules/data-pages/icdom_SubsetsPage"
-export default Page
--- src/pages/subsets/icdot-subsets.js
+++
@@ -1,2 +0,0 @@
-import Page from "../../modules/data-pages/icdot_SubsetsPage"
-export default Page
