ced404f4d54cf99490c8a6a6c36583f11e0ed326
Michael Baudis <mbaudis@me.com>
2023-03-20 09:43:26 +0100

    hierarchy explainer

--- src/modules/data-pages/cellosaurus_SubsetsPage.js
+++ src/modules/data-pages/cellosaurus_SubsetsPage.js
@@ -12,13 +12,17 @@ export default function cellosaurus_SubsetsPage() {
       <div className="content">
         <p>
           The cancer cell lines in <i>cancercelllines.org</i> are labeled
-          by their Cellosaurus IDs as the primary identifier.
+          by their Cellosaurus IDs as the primary identifier. Cell lines are
+          arranged hierarchially: Daughter cell lines are displayed below the
+          primary cell lines they were derived from; <i>i.e.</i> <b>HeLa S3 (CVCL_0058)</b>
+          is shown as a daughter cell line of <b>HeLa  (CVCL_0030)</b> and so forth.
         </p>
         <p>
           Sample selection follows a hierarchical system in which samples
           matching the child terms of a selected class are included in the
           response. This means that one can retrieve all instances and daughter
-          cell lines of a given cell line in a id-based search.
+          cell lines of a given cell line in a id-based search (<i>i.e.</i> searching for
+          HeLa will also return the daughter lines by default - but optional).
         </p>
       </div>
       <Panel heading="Cell Lines (with parental/derived hierarchies)" className="content">
