7d856158c82ca31b101ef42efa1b29302b934f77
Michael Baudis <675030+mbaudis@users.noreply.github.com>
2023-08-23 13:26:16 +0200

    Update AncestryData.js

--- src/components/AncestryData.js
+++ src/components/AncestryData.js
@@ -1,60 +1,40 @@
-import React from "react";
-import { VictoryContainer, VictoryPie, VictoryLabel } from "victory";
+import React, { useState, useCallback } from "react"
+import {VictoryPie, VictoryLegend } from "victory";
 
-export function AncestryData({individual}) {
+export function AncestryData({ individual }) {
+    const processedData = individual?.genomeAncestry
+        ?.sort((a, b) => a.label.localeCompare(b.label)) // Sort alphabetically
+        ?.filter((datum) => parseFloat(datum.percentage) > 0); // Filter out data points with percentage of 0
+    const legendData = processedData.map((datum) => ({ name: `${datum.label} ${datum.percentage}%` }))
+    const colorScale = ['#E0BBE4', '#957DAD', '#D291BC', '#FEC8D8', '#FFDFD3', '#FEE1E8', '#D3C2CE']
+    const [boundingRect, setBoundingRect] = useState({ width: 0, height: 0 });
+    const containerRef = useCallback((node) => {
+        if (node !== null) { setBoundingRect(node.getBoundingClientRect()); }
+    }, []);
 
-  return (
-    <>
-
-  {individual?.genomeAncestry && individual?.genomeAncestry?.length > 0 && (
-    <div style={{ display: "flex", flexDirection: "row", alignItems: "flex-start", width: "100%", marginBottom: "0px" }}>
-      <div style={{ width: "50%" }}>
-          <table style={{ width: "80%", fontSize: "0.9em" }}>
-            <thead>
-            <tr>
-              <th>ID</th>
-              <th>Description</th>
-              <th>%</th>
-            </tr>
-            </thead>
-            <tbody>
-            {individual.genomeAncestry
-                .sort((a, b) => a.label.localeCompare(b.label)) // Sort alphabetically
-                .map((genomeAncestry, key) => {
-                  return (
-                      <tr key={key}>
-                        <td>{genomeAncestry.id}</td>
-                        <td>{genomeAncestry.label}</td>
-                        <td>{genomeAncestry.percentage}</td>
-                      </tr>
-                  )
-                })}
-            </tbody>
-          </table>
-        </div>
-        <div style={{ width: "40%", marginTop: "-70px", marginBottom: "-40px"}}>
-          <VictoryPie
-              data={individual.genomeAncestry
-                  .sort((a, b) => a.label.localeCompare(b.label)) // Sort alphabetically
-                  .filter((datum) => parseFloat(datum.percentage) > 0) // Filter out data points with percentage of 0
-              }
-              x="label"
-              y={(datum) => parseFloat(datum.percentage)}
-              padAngle={2}
-              radius={70}
-              colorScale={['#E0BBE4', '#957DAD', '#D291BC', '#FEC8D8', '#FFDFD3', '#FEE1E8', '#D3C2CE']}
-              labelRadius={({ radius }) => radius + 20}
-              labelComponent={
-                <VictoryLabel
-                    style={{ fontSize: 12 }}
-                    text={({ datum }) => datum.label} // Only show label text if percentage is greater than 0
-                />
-              }
-              containerComponent={<VictoryContainer responsive={false}/>}
-          />
-      </div>
-    </div>
-  )}
-  </>
-  )
+    return (
+        <>
+            {processedData && processedData.length > 0 && (
+                <div ref={containerRef} style={{ display: "flex", flexDirection: "row", alignItems: "flex-start", width: "100%", marginBottom: "0px" }}>
+                    <VictoryPie
+                        data={processedData}
+                        x="label"
+                        y={(datum) => parseFloat(datum.percentage)}
+                        radius={boundingRect.height * 0.35}
+                        colorScale={colorScale}
+                        height={boundingRect.height * 0.9}
+                        width={boundingRect.width * 0.45}
+                        style={{ labels: { display: "none" } }}
+                    />
+                    <VictoryLegend
+                        data={legendData}
+                        style={{ labels: { fontSize: 12} }}
+                        colorScale={colorScale}
+                        height={boundingRect.height * 0.9}
+                        width={boundingRect.width * 0.45}
+                    />
+                </div>
+            )}
+        </>
+    );
 }
