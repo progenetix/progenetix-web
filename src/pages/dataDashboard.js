d16ed7299652cbf0e6d36106f8583b6554866b27
Michael Baudis <m@baud.is>
2025-12-18 11:07:39 +0100

    Update dataDashboard.js

--- src/pages/dataDashboard.js
+++ src/pages/dataDashboard.js
@@ -10,11 +10,10 @@ export default function StatsPage({summaryResults, counts}) {
 
 
   const title = `${DATASETDEFAULT} Data Content Overview`
-  const leadText = `<p>This page shows some data statistics for the ${DATASETDEFAULT} dataset. Please
-allow for some loading time.</p>
-<p>The summary statistics are based on a test version of the Beacon v2 aggregation
+  const leadText = `This page shows some data statistics for the ${DATASETDEFAULT} dataset.
+  Please allow for some loading time. The summary statistics are based on a test version of the Beacon v2 aggregation
 API and are subject to change. Particularly numbers might be incomplete and do not
-reflect the full dataset content at this time.</p>`
+reflect the full dataset content at this time.`
 
   return (
 <Layout title={title} headline={title} leadPanelMarkdown={leadText}>
