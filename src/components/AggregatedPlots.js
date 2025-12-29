50f8e95a6d109e72d25f51df8674a0c4b8b9a8af
Michael Baudis <m@baud.is>
2025-12-29 12:03:36 +0100

    website aggregation data JS code refactoring

--- src/components/AggregatedPlots.js
+++ src/components/AggregatedPlots.js
@@ -1,23 +1,34 @@
 import React, { useState, useCallback } from "react";
 import dynamic from "next/dynamic";
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false, })
+<<<<<<< Updated upstream
 // import {PlotParams} from 'react-plotly.js';
+=======
+import MakeTraces from "./AggregationData";
+
+>>>>>>> Stashed changes
 //----------------------------------------------------------------------------//
 
-var col_no = 30
+var colNo = 20
 
 //----------------------------------------------------------------------------//
 
 export function AggregatedPlots({ summaryResults, filterUnknowns }) {
+
+    const filterOthers = false //true
+
     return (
         <>
         {summaryResults ? 
             summaryResults.map((r) => (
                 <>
                 {r["concepts"].length > 0 && (
-                    <AggregatedStackedPlot agg={r} filterUnknowns={filterUnknowns} />
-                )
-                }
+                    <AggregationPlot
+                        agg={r}
+                        filterUnknowns={filterUnknowns}
+                        filterOthers={filterOthers}
+                    />
+                )}
                 </>
             )
         ) : (
@@ -29,6 +40,7 @@ export function AggregatedPlots({ summaryResults, filterUnknowns }) {
 
 //----------------------------------------------------------------------------//
 
+<<<<<<< Updated upstream
 function AggregatedStackedPlot({ agg, filterUnknowns, filterOthers }) {
 
     var keyedFirst = {}
@@ -162,17 +174,22 @@ function AggregatedStackedPlot({ agg, filterUnknowns, filterOthers }) {
         }
         tracesData.push(thisTrace);
     })
+=======
+function AggregationPlot({ agg, filterUnknowns, filterOthers }) {
+>>>>>>> Stashed changes
 
+    let {tracesData} = MakeTraces({ agg, filterUnknowns, filterOthers, colNo });
 
     const [boundingRect, setBoundingRect] = useState({ width: 0, height: 0 });
     const containerRef = useCallback((node) => {
         if (node !== null) { setBoundingRect(node.getBoundingClientRect()); }
     }, []);
 
-    var outer_w = boundingRect.width
+    const outer_w = boundingRect.width
 
     return (
         <>
+<<<<<<< Updated upstream
             {dist.length > 0 ? (
                 <div ref={containerRef} style={{ display: "flex", flexDirection: "row", alignItems: "flex-start", width: "100%", marginBottom: "0px" }}>
                    <>
@@ -184,6 +201,19 @@ function AggregatedStackedPlot({ agg, filterUnknowns, filterOthers }) {
                     ) : (
                         <StackedPlotlyBar
                             tracesData={tracesData} outer_w={outer_w} title={agg_l}
+=======
+            {tracesData[0].x.length > 0 ? (
+                <div ref={containerRef} style={{ display: "flex", flexDirection: "row", alignItems: "flex-start", width: "100%", marginBottom: "0px" }}>
+                   <>
+                   {/*The following has to be defined - avoiding here the incomplete pie definitions */}
+                   {tracesData[0].x.length <= 8 && tracesData.length < 2 && !agg["sorted"] ? (
+                        <SimplePlotlyPie
+                            tracesData={tracesData} outer_w={outer_w} title={agg["label"]}
+                        />
+                    ) : (
+                        <StackedPlotlyBar
+                            tracesData={tracesData} outer_w={outer_w} title={agg["label"]}
+>>>>>>> Stashed changes
                         />
                     )}
                    </>
@@ -220,10 +250,30 @@ function SimplePlotlyPie({ tracesData, outer_w, title}) { //, title
 //----------------------------------------------------------------------------//
 
 function StackedPlotlyBar({ tracesData, outer_w, title}) { //, title
+<<<<<<< Updated upstream
     return (
       <Plot
         data={tracesData}
         layout={ {barmode: 'stack', width: outer_w, height: 240, title: {text: title}} }
+=======
+    for (let trace of tracesData) {
+        trace.type = 'bar';
+    }
+    return (
+      <Plot
+        data={tracesData}
+        layout={
+            {
+                barmode: 'stack',
+                width: outer_w,
+                height: 240,
+                title: {text: title},
+                yaxis2: {
+                    side: 'right'
+                }
+            }
+        }
+>>>>>>> Stashed changes
       />
     );
 }
